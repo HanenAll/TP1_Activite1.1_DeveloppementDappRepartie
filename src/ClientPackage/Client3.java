6ad858c8766222e9137a7e3b64b5cf909b9015ec
Hanen Allouche <106692503+HanenAll@users.noreply.github.com>
2023-10-22 23:03:34 +0100

    Update

--- src/ClientPackage/Client3.java
+++ src/ClientPackage/Client3.java
@@ -1,7 +1,6 @@
 package ClientPackage;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -13,7 +12,7 @@ public class Client3 {
 		try{
 			System.out.println("je suis un client");
 			//Crée une instance de la classe InetAddress pour représenter l'adresse IP "10.25.14.51"
-		    InetAddress inetAddress = InetAddress.getByName("192.168.100.11"); // chercher moi la machine qui a une @ip 10.25.14.51
+		    InetAddress inetAddress = InetAddress.getByName("localhost"); // chercher moi la machine qui a une @ip 10.25.14.51
 		    InetSocketAddress inetSocketAdd = new InetSocketAddress(inetAddress,1234); //inetSocketAdd lien virtuelle entre les 2 machines 
 		    Socket socket = new Socket();//socket va etre cree qui signifie un transporteur de données 
 		    socket.connect(inetSocketAdd);//socket va connecter sur le chemin des 2 machines 
@@ -23,21 +22,21 @@ public class Client3 {
 		    //Récupère un flux de sortie à partir du socket client. Cela permettra au client d'envoyer des données au serveur
 		    OutputStream os = socket.getOutputStream(); 
 		    Scanner scanner = new Scanner(System.in);//Utilise un objet Scanner pour lire un entier
-		    PrintWriter pw= new PrintWriter(os,true);//PrintWriter nous permet d'envoyer la chaine de caractere caractere par caractere
 		    int nb;
 		    System.out.println("nb= "); 
 		     nb=scanner.nextInt();
-		    String op;
-		    do {
-		    	System.out.println("choisir l'operateur (+,-,*,/) ");
-		    	op=scanner.next(); // nextLine elle lu un String
-		    }
-		    while(!op.equals("+")&& !op.equals("-")&& !op.equals("*")&& !op.equals("/"));
+		    int op;
+		    //le client choisit la nature de l’opération à exécuter côté serveur à travers un Menu de choix.
+		    do
+		    {
+		    System.out.println("Choisissez la nature de l opération: 1-Addition / 2-Soustraction / 3-Multiplication / 4-Division");
+		    op=scanner.nextInt(); // nextLine pour lire un caractère ou une chaine de caractères
+		    } while (op < 1 || op > 4 );
 		    os.write(nb); //Envoie le nombre nb au serveur via le flux de sortie.
-		    pw.println(op);//pw elle concatine le type par defaut en string 
+		    os.write(op);//Envoie l'operation au serveur via le flux de sortie.
 		    int res =is.read(); //Lit la réponses du serveur à partir du flux d'entrée et la stocke dans res
 		    //Affiche le nombre reçu du serveur.
-		    System.out.println(nb+""+op+"5 ="+res); 
+		    System.out.println("le res est="+res); 
 		    System.out.println("deconnexion client");
 	        socket.close();
 		}
